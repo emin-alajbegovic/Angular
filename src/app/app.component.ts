cee552eaea9f5840cde6b3fed61f949b23808652
Emin Alajbegović <emin.alajbegovic@edu.fit.ba>
2021-03-17 10:57:36 +0100

    password-generator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,53 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-password-generator';
+  password='';
+  length=0;
+  includeLetters=false;
+  includeNumbers=false;
+  includeSymbols=false;
+
+  onLengthchange(value:string){
+    const parsedV=parseInt(value);
+    
+    if(!isNaN(parsedV)){
+      this.length=parsedV;
+    }
+  }
+
+  onLetterchange(){
+    this.includeLetters=!this.includeLetters;
+  }
+
+  onNumberchange(){
+    this.includeNumbers=!this.includeNumbers;
+  }
+
+  onSymbolchange(){
+    this.includeSymbols=!this.includeSymbols;
+  }
+
+  onButtonClick(){
+   const numbers='1234567890';
+   const letters='abcdefghijklmnopqrstuvwxyz';
+   const symbols='!@"#$%&/()=?*';
+   
+   let validChars='';
+
+   if(this.includeLetters){
+     validChars+=letters;
+    }
+   if(this.includeNumbers){
+    validChars+=numbers;
+    }
+  if(this.includeSymbols){
+    validChars+=symbols;
+    }
+  let generatePassword='';
+  for(let i=1;i<=this.length;i++){
+        const index=Math.floor(Math.random()*validChars.length);
+        generatePassword+=validChars[index];
+    }
+    this.password=generatePassword;
+  }
 }
